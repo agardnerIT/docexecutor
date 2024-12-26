3634dd6e6a6227f4a5d43600731a81eef0cd4960
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-12-26 07:59:09 +0000

    update

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
 pip install -r $CODESPACE_VSCODE_FOLDER/requirements.txt
-
-# Set secret key to /tmp/secret
-python $CODESPACE_VSCODE_FOLDER/set_secret_key.py
-
-nohup fastapi run $CODESPACE_VSCODE_FOLDER/listenserver.py &
-
-# open listenserver port 8000 publicly
-gh codespace ports visibility 8000:public -c $CODESPACE_NAME
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-export FOO2=bar
+# Set secret key to /tmp/secret
+python $CODESPACE_VSCODE_FOLDER/set_secret_key.py
+
+nohup fastapi run $CODESPACE_VSCODE_FOLDER/listenserver.py &
+
+# open listenserver port 8000 publicly
 gh codespace ports visibility 8000:public -c $CODESPACE_NAME
