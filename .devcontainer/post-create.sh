9f2829e3cb2360d6635430cfb8ca941308f093d8
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-12-26 06:02:08 +0000

    update

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,3 +3,6 @@
 export FOO=bar
 pip install -r requirements.txt
 fastapi run listenserver.py
+
+python set_secret_key.py
+zsh ~/.zshrc .
---
+++ set_secret_key.py
@@ -0,0 +1,22 @@
+import secrets
+import os
+
+# This script runs autonomously at startup
+# A user should never have to manually run this script
+# Generate a random, URL safe
+# 8 byte string and append it to the end of ~/.zshrc
+# post-create.sh then sources that file
+# So the new env var should be available via `printenv`
+# and the secret is never persisted to disk
+# The user will be asked to retrieve this random value: `printenv | grep SECRET_KEY`
+# and use this to "authenticate" the browser docs
+# to this codespace
+
+current_user = os.environ.get("USER","")
+if current_user == "":
+    exit
+
+value =f"SECRET_KEY={secrets.token_urlsafe(8)}\n"
+
+with open(file=f"/home/{current_user}/.zshrc", mode="a") as secret_file:
+    secret_file.write(value)
